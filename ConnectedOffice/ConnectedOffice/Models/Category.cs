03a3da2f1e5a5bf8538a66a9344d86d92427471e
aaryadevg <aaryadevg@gmail.com>
2022-08-24 19:12:10 +0200

    Created API

---
+++ ConnectedOffice/ConnectedOffice/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ConnectedOffice.Models;
+
+namespace ConnectedOffice.Models
+{
+    public partial class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? CategoryDescription { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
