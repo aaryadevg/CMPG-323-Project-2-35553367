5b3235d17e1df7219ad8c983dc0bd82a2062843f
aaryadevg <aaryadevg@gmail.com>
2022-08-25 17:39:04 +0200

    Added dependency injection

--- ConnectedOffice/ConnectedOffice/Program.cs
+++ ConnectedOffice/ConnectedOffice/Program.cs
@@ -1,9 +1,14 @@
 using ConnectedOffice;
+using ConnectedOffice.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<masterContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
